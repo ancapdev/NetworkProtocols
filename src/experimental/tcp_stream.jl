27c03c6001815058ddbbcef1b9ce7a90f362ce6a
Sergey Fokin <sergey.fokin@graystonetrading.com>
2025-02-06 12:00:47 +0100

    revert, only error on larger retransmits

--- src/experimental/tcp_stream.jl
+++ src/experimental/tcp_stream.jl
@@ -149,6 +149,11 @@ function Base.push!(stream::TCPStream, iheader::IPv4Header, packet::TCPPacket)
             isempty(packet.payload) && return
             @warn "TCP retransmission" src_endpoint dst_endpoint iheader packet
             stream.event_handler(src_endpoint.addr, dst_endpoint.addr, TCPE_RETRANSMIT)
+            # NOTE: this is not invalid, but unusual situation, not currently supported
+            if (theader.seq_num + length(packet.payload)) % UInt32 > src_endpoint.seq
+                @error "TCP retransmission beyond current seq_num" src_endpoint dst_endpoint iheader packet
+                stream.event_handler(src_endpoint.addr, dst_endpoint.addr, TCPE_ERROR)
+            end
             return
         end
 
